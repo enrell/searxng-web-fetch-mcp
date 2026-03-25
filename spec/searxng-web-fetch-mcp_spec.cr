ff6ce3bcfeb6ff1c1d17d96ba74a9cbe0a232549
enrell <enrellsa10@proton.me>
2026-03-25 19:23:15 -0300

    chore: Update expected project version to 0.1.1.

--- spec/searxng-web-fetch-mcp_spec.cr
+++ spec/searxng-web-fetch-mcp_spec.cr
@@ -2,7 +2,7 @@ require "./spec_helper"
 
 describe SearxngWebFetchMcp do
   it "has a version" do
-    SearxngWebFetchMcp::VERSION.should eq("0.1.0")
+    SearxngWebFetchMcp::VERSION.should eq("0.1.1")
   end
 
   it "should log correctly" do
